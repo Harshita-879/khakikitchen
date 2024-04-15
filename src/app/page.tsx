3b47c798f961f56d892f082b993fd393744fcb10
Sunny Patil <patilsunny2303@gmail.com>
2024-04-15 17:36:02 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <p className="text-[#C14627] text-2xl md:text-4xl mt-5 mx-2 lg:mx-0 lg:ml-5 text-center">Get Ready! Something really tasty is </p>
       <p className="text-[#C14627] text-5xl md:text-5xl mt-5 lg:mt-12 font-medium ">COMING SOON </p>
       <p className="text-[#C14627] text-3xl md:text-6xl mt-5 lg:mt-12 font-extrabold ">09&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;05&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2024</p>
-      <div className="w-9/12 lg:w-auto flex flex-row mt-5 gap-10 w-fit items-center justify-center">
+      <div className="w-9/12 lg:w-auto flex flex-row mt-5 gap-10 lg:w-fit items-center justify-center">
         <a href="https://www.youtube.com/@TheKhakiKitchen" target="_blank">
         <Image
         src={youtube}
