ff9d7323f9519aff954d45fe87ffa97f6e6d2be0
Sunny Patil <patilsunny2303@gmail.com>
2024-05-04 13:26:18 +0530

    added typewriter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import youtube from "./components/youtube.svg";
 import instagram from "./components/instagram.svg";
 import whatsapp from "./components/whatsapp.svg";
 import { Poppins } from "next/font/google";
+import TypingEffect from "./components/typerwriter";
+import React from "react";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -28,7 +30,10 @@ export default function Home() {
       width={300}
       alt="Khaki Kitchen" />
       <p className="text-[#C14627] text-2xl md:text-4xl mt-5 mx-2 lg:mx-0 lg:ml-5 text-center">Get Ready! Something really tasty is </p>
-      <p className="text-[#C14627] text-5xl md:text-5xl mt-5 lg:mt-12 font-medium ">COMING SOON </p>
+      
+      <div className="text-[#C14627] text-5xl md:text-5xl mt-5 lg:mt-12 font-medium ">
+      <TypingEffect text="COMING SOON!" />
+       </div>
       <p className="text-[#C14627] text-3xl md:text-6xl mt-5 lg:mt-12 font-extrabold ">09&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;05&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2024</p>
       <div className="w-9/12 lg:w-auto flex flex-row mt-5 gap-10 lg:w-fit items-center justify-center">
         <a href="https://www.youtube.com/@TheKhakiKitchen" target="_blank">
